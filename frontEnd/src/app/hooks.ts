dea97ade50de025e59fba063e75c0a26fbfee682
Yaakov Timsit <timsityaakov@gmail.com>
2026-03-18 08:58:58 +0100

    Feat: Finalisation de la logique API et du Header et Sortout ajout de notes

--- frontEnd/src/app/hooks.ts
+++
@@ -1,8 +0,0 @@
-// Fichier : src/app/hooks.ts (ou hook.ts)
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux"; // 👈 On ajoute "type" ici
-import type { RootState, AppDispatch } from "./store";
-
-// Utilise ces hooks dans toute l'app au lieu des versions de base
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
